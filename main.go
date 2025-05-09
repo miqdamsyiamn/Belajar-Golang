079ee431764ca76e7f91bb46fa8fc34d6db0a5ad
miqdamsyiamn <miqdamsyiamn@gmail.com>
2025-05-09 18:42:24 +0700

    update1

--- main.go
+++ main.go
@@ -22,7 +22,7 @@ func main() {
 		<head>
 			<meta charset="UTF-8">
 			<meta name="viewport" content="width=device-width, initial-scale=1.0">
-			<title> Hello World</title>
+			<title>Hello World gofiber</title>
 			<link rel="stylesheet" href="/css/style.css">
 		</head>
 		<body>
